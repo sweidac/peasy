a5bc494d2ea27c1e2f9efaaabd29a8986c1330b5
Simon Weidacher <17548832+sweidac@users.noreply.github.com>
2023-01-05 20:20:05 +0100

    Adds tests for properties and indentation removal.

--- peasy.test.js
+++ peasy.test.js
@@ -26,8 +26,6 @@ it('creates a deeper html structure', () => {
 		</div>
 	`;
 
-	expect(view.childElementCount).toBe(4);
-	expect(view.childNodes.length).toBe(4); // make sure we have no textnodes from the indentation
 	expect(view.querySelectorAll('span').length).toBe(2);
 	expect(view.querySelector('p').textContent).toBe('paragraph');
 });
@@ -44,4 +42,56 @@ it('works with node interpolations', () => {
 
 	expect(outerView).toBeInstanceOf(HTMLElement);
 	expect(outerView.querySelector('.myuser').textContent).toBe('JohnDoe');
+});
+
+it('works with properties', () => {
+	const view = html`
+		<div class="foo" data-lol="sometext" aria-role="code">
+			<h1>Title</h1>
+			<span>Text</span>
+			<span>Text2</span>
+			<p>paragraph</p>
+		</div>
+	`;
+
+	expect(view.childElementCount).toBe(4);
+	expect(view.childNodes.length).toBe(4); // make sure we have no textnodes from the indentation
+	expect(view.getAttribute('data-lol')).toBe('sometext');
+	expect(view.getAttribute('aria-role')).toBe('code');
+});
+
+describe('tab-indenations removal', () => {
+
+	it('removes basic tab-indentation', () => {
+		const view = html`
+			<div>
+				<h1>Title</h1>
+				<span>Text</span>
+				<span>Text2</span>
+				<p>paragraph</p>
+			</div>
+		`;
+
+		expect(view.childElementCount).toBe(4);
+		expect(view.childNodes.length).toBe(4); // make sure we have no textnodes from the indentation
+	});
+
+	it('removes tab-indentations when used with properties', () => {
+		const view = html`
+			<div	class="foo"
+					data-lol="sometext"
+					aria-role="code">
+				<h1>Title</h1>
+				<span>Text</span>
+				<span>Text2</span>
+				<p>paragraph</p>
+			</div>
+		`;
+
+		expect(view.childElementCount).toBe(4);
+		expect(view.childNodes.length).toBe(4); // make sure we have no textnodes from the indentation
+		expect(view.getAttribute('data-lol')).toBe('sometext');
+		expect(view.getAttribute('aria-role')).toBe('code');
+	});
+
 });
